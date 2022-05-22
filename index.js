9ddfcf1c03d8e4ac2c925d41343465e50cff7807
Omar Faruk <omarfarukesham@gmail.com>
2022-05-22 18:28:42 +0600

    purchase api is ready

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/products/:id', async(req, res)=>{
+            const id = req.params.id
+            console.log(id)
+            const query = {_id: ObjectId(id)}
+            const result = await productCollection.findOne(query)
+            res.send(result)
+        })
+
 
 
 
